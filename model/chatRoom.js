adaf89fa8707ce669abb4df40ea124e382e47aa9
Jhonatan R. Saumeth <jhriverasa@unal.edu.co>
2019-11-03 18:09:13 -0500

    Initial commit

---
+++ model/chatRoom.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/chat')
+.then(db => console.log('db connected'))
+  .catch(err => console.log(err));
+
+var Schema = mongoose.Schema;
+
+
+
+var chatRoomSchema = new Schema({
+  
+  messages: 
+  [
+    { 
+      userID: Number, 
+      message: String, 
+      created_at : { 
+        type : Date, 
+        default: Date.now 
+      }
+    }
+  ],
+
+  users: {
+    buyerID: Number, 
+    sellerID: Number, 
+    _id: false
+  }
+  
+});
+
+var Chat = mongoose.model('Chat', chatRoomSchema);
+
+
+module.exports = Chat;
+
---
+++ server.js
@@ -0,0 +1,100 @@
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var Chat = require("./model/chatRoom");
+
+var port = 3001;
+
+
+app.use(express.json());
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/:id/room', function(req, res){
+  Chat.find( {$or: [ {"users.buyerID": parseInt(req.params.id) }, {"users.sellerID": parseInt(req.params.id)} ]}, function(err, data) {
+    if(err) {
+        res.send('Error al obtener la lista de chat rooms');
+    } else {
+        console.log(data);
+        var result = []
+        for (var i in data) {
+          
+          var user;
+
+          if (data[i].users.sellerID == req.params.id) {
+            user = data[i].users.clientID;
+          } else {
+            user = data[i].users.sellerID;
+          }
+          var chat = {id: data[i]._id, user: user,lastMessage: data[i].messages.pop()};
+          result.push(chat);
+        }
+        res.json(result);
+    }
+
+  });
+});
+
+app.get('/:id_room', function(req, res) {
+  Chat.find({"_id": req.params.id_room}, 'messages',function(err, data) {
+    if(err) {
+      res.send('Error al obtener la lista de mensajes');
+    } else {
+      console.log(data);
+      res.json(data);
+    }
+  });
+});
+
+
+app.post('/room', function (req, res) {
+
+    var chat = new Chat(req.body)
+    chat.save(function(err, data) {
+        if (err){
+            res.send('Error al crear una nueva sala de chat');
+            return console.error(err);
+        }else{
+            console.log(data);
+            res.json(data);
+        }
+    });
+     
+});
+
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('subscribe', function(room) {
+    console.log('joining room', room);
+    socket.join(room);
+  });
+  
+  socket.on('send message', function(data) {
+    console.log('sending room post', data.room);
+  
+    Chat.findById(data.room, function (err, doc) {
+      if (err) {
+    
+      } else {
+        doc.messages.push({"userID": data.userID, "message": data.message});
+        doc.save(function (err, doc) {});
+      }
+    });
+  
+    io.in(data.room).emit('conversation private post', {
+        message: data.message
+    });
+  });
+
+});
+
+
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
